457353a86944b4145835aef436af9a2e5049c139
justGyga <iwantdrink229@gmail.com>
2025-11-23 23:40:02 +0700

    fix lab 2

--- lab-2/run.sh
+++ lab-2/run.sh
@@ -5,4 +5,4 @@ echo "Build and start service-2"
 (cd service-2 && docker-compose up -d --build)
 
 echo "Build and start gateway"
-(cd gateway && docker-compose up -d --build --force-recreate --build)
+(cd gateway && docker-compose up -d --build --force-recreate)
